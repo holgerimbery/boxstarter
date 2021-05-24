32205d7740f929aa2561af4684add82acbc0f826
Holger Imbery <holger@imbery.de>
2021-05-24 20:00:00 +0200

    UPDATE: wsl

--- development.ps1
+++ development.ps1
@@ -1,9 +1,12 @@
 # Installazure-cli
 cinst azure-cli -y
 
+# Windows Subsystems/Features
+choco install Microsoft-Hyper-V-All -source windowsFeatures
+choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
 #install dotnet-5.0-sdk
-cinst dotnet-5.0-sdk -y
+#cinst dotnet-5.0-sdk -y
 
 
 #Install Fonts: SourceCodePro, cascadiacodepl
@@ -46,9 +49,9 @@ code --force --install-extension sohamkamani.code-eol
 
 
 #install node.js lts, python3
-cinst nodejs-lts -y
+#cinst nodejs-lts -y
 cinst python3 -y
-cisnt yarn -y
+#cinst yarn -y
 
 #Install Git
 cinst git --params '"/GitAndUnixToolsOnPath /WindowsTerminal /SChannel"' -y
@@ -70,10 +73,10 @@ Install-Module -Name oh-my-posh
 Update-Module -Name oh-my-posh
 
 #Install ruby
-choco install ruby --version=2.7.0.1 -y 
-cinst msys2 --params "/NoUpdate" -y
-refreshenv
-Write-host "Please install jekyll: `nridk install `nrefreshenv `ngem install jekyll bundler`n`nand execute within project folder:`nbundle install`nbundle exec jekyll serve`n"
+#choco install ruby --version=2.7.0.1 -y 
+#cinst msys2 --params "/NoUpdate" -y
+#refreshenv
+#Write-host "Please install jekyll: `nridk install `nrefreshenv `ngem install jekyll bundler`n`nand execute within project folder:`nbundle install`nbundle exec jekyll serve`n"
 
 
 #Install NuGet Package Explorer
@@ -81,4 +84,4 @@ cinst nugetpackageexplorer -y
 if (test-path (Join-Path ([Environment]::GetFolderPath("Desktop")) "NugetPackageExplorer.exe.lnk")) {
     Move-Item (Join-Path ([Environment]::GetFolderPath("Desktop")) "NugetPackageExplorer.exe.lnk") (Join-Path ([Environment]::GetEnvironmentVariable("AppData")) "Microsoft\Windows\Start Menu\Programs\NugetPackageExplorer.lnk")
 }
-Write-host "Please install: `nhttps://github.com/microsoft/BotFramework-Composer-Nightlies,`nhttps://github.com/microsoft/BotFramework-Emulator"
+#Write-host "Please install: `nhttps://github.com/microsoft/BotFramework-Composer-Nightlies,`nhttps://github.com/microsoft/BotFramework-Emulator"
